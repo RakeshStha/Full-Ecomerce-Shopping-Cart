e3aa45b34041e370f20cb4200f91a98f609169a8
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2020-12-20 10:51:43 +0545

    Filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json"
 
@@ -12,6 +13,43 @@ class App extends React.Component {
         sort:"",
     };
   }
+
+    sortProducts = (event) => {
+      const sort = event.target.value;
+        console.log(event.target.value);
+        this.setState((state)=>({
+          sort:sort,
+          products : this.state.products
+          .slice()
+          .sort((a,b) =>
+            sort === "lowest"
+            ? a.price > b.price
+            ? 1
+            :-1
+            : sort === "highest"
+            ? a.price < b.price
+            ? 1
+            :-1
+            :a._id < b._id
+            ? 1
+            :-1
+          )
+        }))
+    }
+
+    filterProducts= (event) => {
+      console.log(event.target.value);
+      if(event.target.value === ""){
+        this.setState({size: event.target.value, product:data.products});
+      }
+      else{
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0)
+      })
+    }
+    }
+
   render(){
   return (
     <div className="grid-container">
@@ -21,15 +59,21 @@ class App extends React.Component {
       <main>
         <div className="content">
           <div className="main">
+            <Filter count={this.state.products.length}
+            size={this.state.size}
+            sort={this.state.sort}
+            filterProducts={this.filterProducts}
+            sortProducts={this.sortProducts}
+            />
             <Products products={this.state.products}/>
           </div>
           <div className="sidebar">
-            Carts
+            Carts Items
           </div>
         </div>
       </main>
         <footer>
-          All rigth is reserved
+          All right is reserved
         </footer>
       
     </div>
