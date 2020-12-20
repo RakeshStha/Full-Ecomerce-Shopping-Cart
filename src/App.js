18a4ac383095b5c1a73406739b3d4bc69db7aa12
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2020-12-20 19:18:54 +0545

    frontend Localstorage

--- src/App.js
+++ src/App.js
@@ -10,17 +10,24 @@ class App extends React.Component {
     super();
     this.state={
         products: data.products,
-        cartItems:[],
+        cartItems: localStorage.getItem("cartItems") 
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [],
         size:"",
         sort:"",
     };
   }
 
+    createOrder =(order) => {
+      alert("Need to save order for" + order.name);
+    }
+
     removeFromCart = (product) => {
       const cartItems = this.state.cartItems.slice();
       this.setState({
         cartItems: cartItems.filter( (x) => x._id !== product._id),
-      })
+      });
+      localStorage.setItem("cartItems",JSON.stringify(cartItems.filter( (x) => x._id !== product._id)));
       
     }
 
@@ -37,6 +44,7 @@ class App extends React.Component {
         cartItems.push({...product, count: 1 });
       }
       this.setState({cartItems});
+      localStorage.setItem("cartItems",JSON.stringify(cartItems));
     }
 
     sortProducts = (event) => {
@@ -97,7 +105,8 @@ class App extends React.Component {
           </div>
           <div className="sidebar">
             <Cart cartItems={this.state.cartItems} 
-            removeFromCart={this.removeFromCart}/>
+            removeFromCart={this.removeFromCart}
+            createOrder={this.createOrder}/>
           </div>
         </div>
       </main>
