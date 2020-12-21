dab75e6ccf62c11f4a8374a840113a8c80e21b3c
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2020-12-21 22:43:41 +0545

    add redux to filter

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ class App extends React.Component {
   constructor(){
     super();
     this.state={
-        products: data.products,
         cartItems: localStorage.getItem("cartItems") 
         ? JSON.parse(localStorage.getItem("cartItems"))
         : [],
-        size:"",
-        sort:"",
+       
     };
   }
 
